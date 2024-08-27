cdcffc49c0acccf63e3b544cdbb11a872ff47920
trexium <emil@gilljam.org>
2024-08-27 16:25:26 +0200

    Added endpoint to get categories with children.

--- HexedSceneryWebsite/HexedSceneryWebsite/HexedSceneryWebsite/Api/v1/Controllers/CategoryController.cs
+++ HexedSceneryWebsite/HexedSceneryWebsite/HexedSceneryWebsite/Api/v1/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using HexedSceneryData.Models;
 using HexedSceneryWebsite.Api.v1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,12 +19,17 @@ namespace HexedSceneryWebsite.Api.v1.Controllers
             _context = context;
         }
 
-        // GET: api/<CategoryController>
         [HttpGet]
         public IEnumerable<TableCategory> Get()
         {
             //return new string[] { "value1", "value2" };
             return _context.TableCategories.Where(m => m.Active == true && !string.IsNullOrEmpty(m.DisplayName)).ToList();
+        }
+
+        [HttpGet("includeChildren")]
+        public IEnumerable<TableCategory> GetWithChildren()
+        {
+            return _context.TableCategories.Include(m => m.EncounterTypes).Where(m => m.Active == true);
         }
     }
 }
